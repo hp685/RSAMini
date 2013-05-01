d1aa4b79d86d757d5a4d6def047b0b9aec98591e
Harsh <harsh@nyu.edu>
2013-05-01 19:24:59 -0400

    restructuring: partial commit

--- rsa.c
+++ rsa.c
@@ -7,7 +7,6 @@
 
 
 
-
 /*May need a sophisticated random generator later*/
 int generateRandomBits(){
 
@@ -108,6 +107,7 @@ struct bit_r* bit_representation(int_r n){
 			strcat(b,"1");
 		n >>= 1;
 
+
    	}
 
   	int start = strlen(b);
@@ -124,6 +124,65 @@ struct bit_r* bit_representation(int_r n){
 	t->sz = MAX_INT_LEN;
 	t->bits = reverse(t->bits, t->sz);
 	return t;
+}
+
+
+
+/* Generates candidate primes according to the spec*/
+struct bit_r* generateRandomNumbers(){
+
+	/*Pad the leading bits*/
+	char *prefix = '00000000000000000000001';
+	int i;
+	struct bit_r *numbers = (struct bit_r*) malloc(sizeof(struct bit_r*));
+	numbers->bits = (char*) malloc(sizeof(char* ) * MAX_INT_LEN);
+
+	for(i = 0; i < MAX_BITS; i++){
+
+		char bit = itoa(generateRandomBits());
+		strcat(prefix,bit);
+	}
+
+	strcat(prefix,'1');
+	numbers->bits = prefix;
+	numbers->sz   = MAX_INT_LEN;
+	return numbers
+}
+
+/*Takes a stream of bits and outputs an integer*/
+int_r bits_to_int_r(struct bit_r* numbers ){
+
+	int i;
+	int_r number = 0;
+	for(i = 0; i < strlen(numbers->bits); i++){
+
+		if(numbers->bits[i] == '1' ){
+			number <<= 1;
+		}
+	}
+
+	return number;
+}
+
+
+/*Check with twenty values of a*/
+/*Should always return a prime(whp)*/
+int_r candidate_primes(){
+
+	int i;
+	int_r n = bits_to_int_r(generateRandomNumbers());
+	int pass = 0;
+
+	for(i = 0; i < NUM_CHECKS; i++){
+
+		int_r a = bits_to_int_r(generateRandomBits());
+		pass = isPrime(a,n);
+
+		if(!pass)
+			break;
+	}
+
+	return pass == 1 ? n : candidate_primes();
 }
 
 
